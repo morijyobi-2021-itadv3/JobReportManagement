b7bc272ea67199b0a532b82b7f535d97bd82265b
youhei takahashi <y.takahashi@morijyobi.ac.jp>
2021-11-15 09:47:54 +0900

    fix RV

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def top():
-    samples = db.select_sample()
-    return render_template('index.html', samples=samples)
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
