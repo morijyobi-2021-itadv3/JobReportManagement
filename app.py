fd897f74d38689b9098625c640c28b2c3851c764
hikaru0112 <h.sato.sys19@morijyobi.ac.jp>
2021-11-25 12:16:22 +0900

    ルートでの値取得

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def top():
-    return render_template('index.html')
+    
+    return render_template('index.html',data = select_sample())
 
 if __name__ == "__main__":
     app.run(debug=True)
