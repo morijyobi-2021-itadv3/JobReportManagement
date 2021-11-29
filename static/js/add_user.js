96629912813c7799d9674b54aa7683bd76ea1246
tora87 <t.hanasaka.sys19@morijyobi.ac.jp>
2021-11-29 11:29:28 +0900

    [wip]commit to change branch

--- static/js/add_user.js
+++ static/js/add_user.js
@@ -4,8 +4,20 @@ document.addEventListener('DOMContentLoaded',() => {
   const thead = document.querySelector('.add-user-table thead')
   const thead_line = document.querySelector('.add-user-table thead tr')
   const tbody = document.querySelector('.add-user-table tbody')
-  const selectbox = document.getElementById('department-selector')
+  const selectbox = document.querySelector('#department-selector')
+  const user_type_radioBtn = document.getElementsByName('user-type')
   const fileReader = new FileReader()
+
+  let selected_user = user_type_radioBtn[0].value
+
+  user_type_radioBtn.forEach(item => {
+    item.addEventListener('change',() => {
+      if(item.checked) selected_user = item.value
+      console.log(selected_user)
+    })
+  })
+
+  console.log(selected_user)
 
   selectbox.addEventListener('change', () => {
     if(selectbox.value === '学科・コースを選択'){
@@ -34,7 +46,6 @@ document.addEventListener('DOMContentLoaded',() => {
         let key = header[index];
         result[key] = datas[index];
       }
-
       return result
     })
 
@@ -42,24 +53,62 @@ document.addEventListener('DOMContentLoaded',() => {
 
     if(items) {
       table.style.display = 'table'
-      //テーブルヘッダーを追加
+      thead_line.innerHTML = `<th></th>`
       header.map(item => thead_line.innerHTML += `<th>${item}</th>`)
-
+      
       items.map((csvData,index) => {
         tbody.innerHTML += `<tr class="user-data-${index}"></tr>`
         const data_line = document.querySelector(`.user-data-${index}`)
-        let thInnerHtml = ''
-        
-        if(check(csvData).length !== 0) {
-          console.log('Error箇所があります')
-        }
-        
+        let checkTdHtml = ''
+        allCheck = true
+        //createElement,appendChildを使う方法
+
         for(const type in csvData){
-          data_line.innerHTML += `
-            <td>${csvData[type]}</td>
-          `
+          let td = document.createElement('td')
+          if(!checkTest(csvData[type],type)){
+            allCheck = false
+            td.classList.add('error')
+          }
+
+          td.innerText = csvData[type]
+          data_line.appendChild(td)
         }
+
+        checkTdHtml = allCheck ? 
+        `
+        <td>
+          <span class="material-icons-outlined success">check</span>
+        </td>
+        `
+        :
+        `
+        <td>
+          <span class="material-icons-outlined error">error_outline</span>
+        </td>
+        `
+
+        data_line.insertAdjacentHTML('afterbegin',checkTdHtml)
         
+        // if(check(csvData).length !== 0) {
+        //   console.log('Error項目があります')
+        //   checkTdHtml = `
+        //   <td>
+        //     <span class="material-icons-outlined error">error_outline</span>
+        //   </td>
+        //   `
+        // }else{
+        //   checkTdHtml = `
+        //   <td>
+        //     <span class="material-icons-outlined success">check</span>
+        //   </td>
+        //   `
+        // }
+        
+        // for(const type in csvData){
+        //   data_line.innerHTML += `
+        //     <td>${csvData[type]}</td>
+        //   `
+        // }
       })
     }
 
@@ -81,22 +130,42 @@ document.addEventListener('DOMContentLoaded',() => {
   })
 
   //バリデーションチェック用関数
-  const check = (datas) => {
-    let errorMessages = []
-    for(const type in datas) {
-      switch(type) {
-        case '学籍番号':
-          checkStudentNumber(datas[type],errorMessages)
-          break
-        case '氏名':
-          checkName(datas[type],errorMessages)
-          break
-        case 'メールアドレス':
-          checkMail(datas[type],errorMessages)
-          break
-      }
+  // const check = (datas) => {
+  //   let errorMessages = []
+  //   for(const type in datas) {
+  //     switch(type) {
+  //       case '学籍番号':
+  //         checkStudentNumber(datas[type],errorMessages)
+  //         break
+  //       case '氏名':
+  //         checkName(datas[type],errorMessages)
+  //         break
+  //       case 'メールアドレス':
+  //         checkMail(datas[type],errorMessages)
+  //         break
+  //     }
+  //   }
+  //   return errorMessages
+  // }
+
+  const checkTest = (data,type) => {
+    let check
+    switch(type) {
+      case '学籍番号':
+        check = checkStudentNumberTest(data)
+        break
+      case '氏名':
+        check = checkNameTest(data)
+        break
+      case 'メールアドレス':
+        check = checkMailTest(data)
+        break
+      default:
+        check = true
+        break
     }
-    return errorMessages
+
+    return check
   }
 
   const checkStudentNumber = (number,errorMessages) => {
@@ -112,4 +181,10 @@ document.addEventListener('DOMContentLoaded',() => {
     const regex = new RegExp(/^[a-z]+\.[a-z]+\.sys[0-9]{2}@morijyobi\.ac\.jp$/)
     if(!regex.test(mail)) errorMessages.push('メールアドレスが適切ではありません。')
   }
+
+  const checkStudentNumberTest = number => new RegExp(/^[0-9]{６}$/).test(number)
+
+  const checkNameTest = name => name.length <= 64
+
+  const checkMailTest = mail => new RegExp(/^[a-z]+\.[a-z]+\.sys[0-9]{2}@morijyobi\.ac\.jp$/).test(mail)
 })
