fbb7a5a08f31a352c1cc47d1d7434c4dca113508
hikaru0112 <h.sato.sys19@morijyobi.ac.jp>
2021-12-07 03:17:04 +0900

    db.pyの環境変数の取得方法の変更

--- model/db.py
+++ model/db.py
@@ -1,19 +1,38 @@
 import psycopg2
+import urllib.parse
+import os
 
-def select_sample():
-    conn = psycopg2.connect(
-        host = '',
-        port = 5432,
-        user = '',
-        database = '',
-        password = ''
+
+def get_connection():
+    """DBのコネクションを返すメソッド
+    Args:
+        なし
+    Returns:
+        成功時:Connection
+        失敗時:None
+    
+    環境変数(DATABASE_URL)を使用します。
+
+    環境変数の構成:
+        "postgresql://{ホスト名}:{ポート番号}/{DB名}?user={ユーザ名}&password={パスワード}"
+
+    """
+    try:
+        url = urllib.parse.urlparse(os.environ["DATABASE_URL"])
+        conn = psycopg2.connect(
+        database=url.path[1:],
+       
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
     )
-
-    cur = conn.cursor()
-    cur.execute('SELECT * FROM sample;')
-    results = cur.fetchall()
-
-    cur.close()
-    conn.close()
-
-    return results
+        
+        return conn
+    except KeyError:
+        print("環境変数の設定がされていません")
+    except psycopg2.OperationalError:
+        print("DBに接続できません")
+    except Exception:
+        print("エラーが発生しました。")
+ 
