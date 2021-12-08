139ead224f4bac03b97fe59fa74348866b5bc38a
tora87 <t.hanasaka.sys19@morijyobi.ac.jp>
2021-12-08 11:21:46 +0900

    [wip]CSVファイルを取得し二次元配列に格納する

--- view/teacher.py
+++ view/teacher.py
@@ -1,6 +1,6 @@
 from flask import Flask, Blueprint, render_template, request
 import csv
-
+import io
 
 teacher_bp = Blueprint('teacher',__name__, url_prefix='/teacher')
 
@@ -9,8 +9,17 @@ def send_first_page():
   if(request.method == 'GET'):
     return render_template('add_user.html')
   else:
-    csvdata = request.files['csv']
-    result = csv.reader(csvdata)
-    for data in result:
-      print(data)
+    csvdata = request.files.get('csv')
+    result = []
+    for row in csvdata:
+      rowdata = [data.strip() for data in row.decode(encoding='shift-jis').split(',')]
+      print(rowdata)
+      
+
+    # print(csvdata)
+    # print(type(csvdata.stream))
+    # text_stream = io.TextIOWrapper(csvdata.stream, encoding="utf-8")
+    # for data in csv.reader(text_stream):
+    #   print(data)
+
     return render_template('index.html')
