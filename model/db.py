622eb140a4e3fef46440f15252ddcfaf5a59e70e
hikaru0112 <h.sato.sys19@morijyobi.ac.jp>
2021-12-10 09:52:08 +0900

    無駄な改行の削除

--- model/db.py
+++ model/db.py
@@ -21,7 +21,6 @@ def get_connection():
         url = urllib.parse.urlparse(os.environ["DATABASE_URL"])
         conn = psycopg2.connect(
         database=url.path[1:],
-       
         user=url.username,
         password=url.password,
         host=url.hostname,
