f38a001a1ede8313fee75baf94fc3a0ac346c3b0
tora87 <t.hanasaka.sys19@morijyobi.ac.jp>
2021-12-10 23:00:14 +0900

    DB(仮)から学科コース名を取得しプルダウンデータとして追加する処理

--- model/db.py
+++ model/db.py
@@ -1,16 +1,20 @@
 import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv(override=True)
 
 def select_sample():
     conn = psycopg2.connect(
         host = '',
-        port = 5432,
-        user = '',
-        database = '',
+        port = '',
+        user = os.getenv('PG_USER'),
+        database = os.getenv('PG_DATABASE'),
         password = ''
     )
 
     cur = conn.cursor()
-    cur.execute('SELECT * FROM sample;')
+    cur.execute('SELECT * FROM departments;')
     results = cur.fetchall()
 
     cur.close()
--- view/teacher.py
+++ view/teacher.py
@@ -1,25 +1,18 @@
 from flask import Flask, Blueprint, render_template, request
 import csv
 import io
-
+from model.db import select_sample
 teacher_bp = Blueprint('teacher',__name__, url_prefix='/teacher')
 
 @teacher_bp.route('/add_user',methods=['GET','POST'])
 def send_first_page():
   if(request.method == 'GET'):
-    return render_template('add_user.html')
+    # 学科コース名を取得する
+    departments = select_sample()
+    return render_template('add_user.html',departments=departments)
   else:
     csvdata = request.files.get('csv')
-    result = []
-    for row in csvdata:
-      rowdata = [data.strip() for data in row.decode(encoding='shift-jis').split(',')]
-      print(rowdata)
-      
-
-    # print(csvdata)
-    # print(type(csvdata.stream))
-    # text_stream = io.TextIOWrapper(csvdata.stream, encoding="utf-8")
-    # for data in csv.reader(text_stream):
-    #   print(data)
+    # 取得したCSVデータを二次元配列に格納
+    csvDataMatrix = [[data.strip() for data in row.decode(encoding='shift-jis').split(',')] for row in csvdata]
 
     return render_template('index.html')
