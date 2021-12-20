102d76a8608501c87b616a5f851b812ae451d690
tora87 <t.hanasaka.sys19@morijyobi.ac.jp>
2021-12-20 21:30:23 +0900

    students.pyファイル作成,学生ユーザー追加関数作成,ファイル名変更

---
+++ model/students.py
@@ -0,0 +1,25 @@
+from model.db import get_connection
+
+conn = get_connection()
+
+def insert_new_student(user_id,graduation_year,student_number,department_id,teacher_id):
+  """
+  新しいユーザーの追加
+    Args:
+      user_id(int): 紐づくユーザーID
+      graduation_year(int): 卒業年度
+      student_number(int): 学生番号
+      department_id(int): 紐づく学科ID
+      teacher_id(int): 紐づく教員(担任)ID
+    Returns:
+      bool: 成功したかどうか
+  """
+  sql = 'INSERT INTO students VALUES (%s,%s,%s,%s,%s);'
+
+  cur = conn.cursor()
+  results = cur.execute(sql,[user_id,graduation_year,student_number,department_id,teacher_id])
+
+  cur.close()
+  conn.close()
+
+  return results
