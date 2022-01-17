bec6c5250a45bbb38d2cd9d20ab4cb6140ec480a
kentamjb <kenta.sato.sys19@morijyobi.ac.jp>
2022-01-17 10:21:34 +0900

    [delete]余計なコメントの削除

--- model/report.py
+++ model/report.py
@@ -5,19 +5,6 @@ import os
 from model.db import get_connection
 
 def select_all():
-    """DBのコネクションを返すメソッド
-    Args:
-        なし
-    Returns:
-        成功時:Connection
-        失敗時:None
-    
-    環境変数(DATABASE_URL)を使用します。
-
-    環境変数の構成:
-        "postgresql://{ホスト名}:{ポート番号}/{DB名}?user={ユーザ名}&password={パスワード}"
-
-    """
     try:
         conn = get_connection()
         cur = conn.cursor()
