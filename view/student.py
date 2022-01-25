02eefba4525d5ccf8d5925e0b40306a36bcd651b
kentamjb <kenta.sato.sys19@morijyobi.ac.jp>
2022-01-25 11:28:13 +0900

    [add]詳細ページへ飛ぶルートの作成と重複したデータの削除

--- view/student.py
+++ view/student.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from model.report import select_all
 
 student = Blueprint('student', __name__, url_prefix='/student')
@@ -6,5 +6,21 @@ student = Blueprint('student', __name__, url_prefix='/student')
 @student.route('/list')
 def open_list():
     datas = select_all()
-    return render_template('student/report_list.html', datas=datas)
 
+    num = 0
+    reports = []
+
+    while num < len(datas) -1:
+        if datas[num][0] != datas[num+1][0]:
+            reports.append(datas[num])
+        num += 1            
+        
+    reports.append(datas[num])
+
+    return render_template('student/report_list.html', report=reports)
+
+@student.route('/details')
+def sample():
+    id = request.args.get('id') # get送信時
+    # id = request.form.get('id') # post送信時
+    return render_template('student/sample.html', id=id)
