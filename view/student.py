dc1c8f647f66a5521149c5dd5afccd55f1c190bf
kentamjb <kenta.sato.sys19@morijyobi.ac.jp>
2022-01-25 12:34:13 +0900

    [add]get,postメソッドの追加

--- view/student.py
+++ view/student.py
@@ -1,23 +1,32 @@
+from crypt import methods
 from flask import Blueprint, render_template, request
-from model.report import select_all
+from model.report import select_reports
+from model.industries import select_industries
 
 student = Blueprint('student', __name__, url_prefix='/student')
 
-@student.route('/list')
+@student.route('/list', methods=['GET','POST'])
 def open_list():
-    datas = select_all()
+    if request.method == 'GET':
+        datas = select_reports()
 
-    num = 0
-    reports = []
+        num = 0
+        reports = []
 
-    while num < len(datas) -1:
-        if datas[num][0] != datas[num+1][0]:
-            reports.append(datas[num])
-        num += 1            
-        
-    reports.append(datas[num])
+        while num < len(datas) -1:
+            if datas[num][0] != datas[num+1][0]:
+                reports.append(datas[num])
+            num += 1            
+            
+        reports.append(datas[num])
 
-    return render_template('student/report_list.html', report=reports)
+        industry = select_industries();
+
+        return render_template('student/report_list.html', report=reports, industry=industry)
+
+    elif request.method == 'POST':
+        print('aaa')
+
 
 @student.route('/details')
 def sample():
