e0bbaeb77e641f3833056d003c464aef25522c74
kentamjb <kenta.sato.sys19@morijyobi.ac.jp>
2022-01-25 11:27:26 +0900

    [add]重複したデータを削除するためのソート

--- model/report.py
+++ model/report.py
@@ -6,11 +6,13 @@ def select_all():
     try:
         conn = get_connection()
         cur = conn.cursor()
-        cur.execute("SELECT reports.company,tests.stage,departments.name,students.graduation_year,reports.prefecture from reports "
+        cur.execute("SELECT reports.id,reports.company,tests.stage,departments.name,students.graduation_year,reports.prefecture from reports "
                         + "JOIN users ON users.id = reports.student_id "
                         + "JOIN students ON students.id = users.id "
                         + "JOIN departments ON departments.id = students.department_id "
                         + "JOIN tests ON tests.report_id = reports.id "
+                        + "ORDER BY reports.id, tests.stage"
+                        # + "LIMIT 7"
                     )
 
         rows = cur.fetchall()
