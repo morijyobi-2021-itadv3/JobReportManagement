ae4bd43a604f549c5238031a60079821d02af16d
kentamjb <kenta.sato.sys19@morijyobi.ac.jp>
2022-02-04 09:45:05 +0900

    [add]卒業年度を取得するメソッド

--- model/students.py
+++ model/students.py
@@ -1,26 +1,5 @@
-<<<<<<< HEAD
+
 import psycopg2
-
-from model.db import get_connection
-
-def select_graduation_year():
-    try:
-        conn = get_connection()
-        cur = conn.cursor()
-        cur.execute("SELECT DISTINCT ON (graduation_year) graduation_year FROM students")
-
-        rows = cur.fetchall()
-
-        return rows
-
-    except KeyError:
-        print("環境変数の設定がされていません")
-    except psycopg2.OperationalError:
-        print("DBに接続できません")
-    except Exception:
-        print("エラーが発生しました。", Exception)
- 
-=======
 from model.db import get_connection
 
 def insert_new_student(user_id,graduation_year,student_number,department_id,teacher_id):
@@ -48,4 +27,23 @@ def insert_new_student(user_id,graduation_year,student_number,department_id,teac
   conn.close()
 
   return results
->>>>>>> main
+
+def select_graduation_year():
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
+        cur.execute("SELECT DISTINCT ON (graduation_year) graduation_year FROM students")
+
+        rows = cur.fetchall()
+
+        return rows
+
+    except KeyError:
+        print("環境変数の設定がされていません")
+    except psycopg2.OperationalError:
+        print("DBに接続できません")
+    except Exception:
+        print("エラーが発生しました。", Exception)
+
+
+
